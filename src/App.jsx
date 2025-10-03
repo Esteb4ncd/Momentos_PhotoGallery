fa4c3fe648f24cd67338a0d9fc3bca933c46eef9
EmmaJennings <ejennings13@my.bcit.ca>
2025-10-02 23:54:19 -0700

    Working on Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react"
 import SinglePost from "./pages/SinglePost"
 import UploadPost from "./pages/UploadPost"
+import NavBar from "./pages/NavBar"
 
 export default function App() {
 
 
   return (
     <div>
-      <UploadPost />
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <UploadPost />
+      </main>
     </div>
   )
 }
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -2,3 +2,32 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+
+export default function NavBar() {
+    return ( 
+
+        <Box 
+            sx={{ 
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%", 
+                zIndex: 1000,
+                backgroundColor: "lightgray",
+            }}
+        >
+            <Box
+                sx={{ 
+                display: "flex",
+                justifyContent: "flex-start",
+
+            }}>
+                <Typography variant="h2">Mementos</Typography>
+            </Box>
+
+            <Box >
+                <Typography variant="h4">Mementos</Typography>
+            </Box>
+        </Box>
+    )
+}
